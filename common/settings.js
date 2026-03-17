39cd322339d9dc878e6ff1576f11c52d17f9c499
DavidLaPorte <david@davidlaporte.org>
2026-03-17 14:21:11 -0400

    feat: add shared settings module with defaults

---
+++ common/settings.js
@@ -0,0 +1,20 @@
+const EBF_DEFAULTS = {
+  minFeedbackCount: 1,
+  minPositivePercent: 90,
+  filterMode: 'hide',
+  bannerStyle: 'sticky'
+};
+
+async function ebfGetSettings() {
+  return new Promise((resolve) => {
+    chrome.storage.sync.get(EBF_DEFAULTS, (settings) => {
+      resolve(settings);
+    });
+  });
+}
+
+async function ebfSaveSettings(settings) {
+  return new Promise((resolve) => {
+    chrome.storage.sync.set(settings, resolve);
+  });
+}
